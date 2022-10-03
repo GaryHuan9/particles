66196503d975e49d3e555fee9f80c507627ab265
Gary Huang <henhaodegaigai@gmail.com>
2022-10-03 00:01:33 -0400

    Added nice formatting to device detection

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <CL/cl.hpp>
+#include <fmt/format.h>
 
-int main()
+bool find_device(cl::Device& device)
 {
 	std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 
-//	std::cout << std::format("Found {} platforms.\n", platforms.size());
+	std::cout << fmt::format("Found {} platform(s): ", platforms.size());
 
-	// Get available platforms
-//	std::vector<cl::Platform> platforms;
-//
-//	std::cout << platforms.size() << '\n';
-//	std::vector<cl::Device> devices;
-//
-//	platforms[0].getDevices(CL_DEVICE_TYPE_ALL, &devices);
-//
-//	std::cout << devices.size() << '\n';
-//
-//	cl_ulong memory;
-//	devices[0].getInfo(CL_DEVICE_GLOBAL_MEM_SIZE, &memory);
-//
-//	std::cout << memory << '\n';
-//
-//	char vendor[128];
-//	devices[0].getInfo(CL_DEVICE_VENDOR, &vendor);
-//
-//	std::cout << vendor << '\n';
+	if (platforms.empty())
+	{
+		std::cout << "exiting...\n";
+		return false;
+	}
 
+	cl::Platform& platform = platforms[0];
+	std::cout << fmt::format("using {}.\n", platform.getInfo<CL_PLATFORM_NAME>());
+
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+	std::cout << fmt::format("Found {} devices(s): ", devices.size());
+
+	if (devices.empty())
+	{
+		std::cout << "exiting...\n";
+		return false;
+	}
+
+	device = cl::Device{ devices[0] };
+	std::cout << fmt::format("using {} from {} with {} memory and {} compute units.\n",
+	                         device.getInfo<CL_DEVICE_NAME>(),
+	                         device.getInfo<CL_DEVICE_VENDOR>(),
+	                         device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>(),
+	                         device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>());
+
+	return true;
+}
+
+int main()
+{
+	cl::Device device;
+	if (!find_device(device)) return 0;
+
+	
 	return 0;
 }
